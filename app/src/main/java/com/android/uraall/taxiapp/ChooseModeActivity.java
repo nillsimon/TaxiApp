b616796ecb20133885176e8dbf89e0bbdef9ddd7
YouRa <allakhverdovyuriy@gmail.com>
2019-05-21 07:24:27 +0300

    ChooseModeActivity

---
+++ app/src/main/java/com/android/uraall/taxiapp/ChooseModeActivity.java
@@ -0,0 +1,29 @@
+package com.android.uraall.taxiapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                PassengerSignInActivity.class
+        ));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                DriverSignInActivity.class
+        ));
+    }
+}
---
+++ app/src/main/java/com/android/uraall/taxiapp/DriverSignInActivity.java
@@ -0,0 +1,13 @@
+package com.android.uraall.taxiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_in);
+    }
+}
